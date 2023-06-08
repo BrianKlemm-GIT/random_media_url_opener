0c8e0b3c4e9af4e415f2340f2a0c94786c6b1275
Brian Klemm <resetbrian@gmail.com>
2023-06-08 15:58:20 -0700

    created channel class so the user can make their own channels.

---
+++ channel.py
@@ -0,0 +1,23 @@
+import random
+import webbrowser
+
+
+class Channel:
+    def __init__(self, type, url):
+        self.type = type
+        self.url = url
+
+    def add_url(self):
+        new_url = input("Enter the full URL you would like to add.")
+        self.url.append(new_url)
+
+    def remove_url(self):
+        remove_url = input("Enter the full URL you would like to remove.")
+        self.url.remove(remove_url)
+
+    def play_media(self):
+        video = random.choice(self.url)
+        webbrowser.open(video, new=1)
+
+    def display_urls(self):
+        print(self.url)
